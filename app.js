210bde4434b2e3d5fbfe27dc2b2e8ef5a415c1aa
kimtaegwan <ab154@naver.com>
2022-08-21 19:11:01 +0900

    middleware study

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   // use를 사용하면 새로운 미들웨어를 추가할 수 있음.
-  console.log("In the middleware");
-  next(); // next 요청으로 다음 라인에 있는 미들웨어 실행
+  console.log("In another middleware");
+  res.send('<h1>The "Add product" Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   // use를 사용하면 새로운 미들웨어를 추가할 수 있음.
   console.log("In another middleware");
   res.send("<h1>Hello from Express!</h1>");
