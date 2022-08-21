ab305804690886971c05ca0cb508629833689308
kimtaegwan <ab154@naver.com>
2022-08-21 19:51:18 +0900

    Add body-parser

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
   // use를 사용하면 새로운 미들웨어를 추가할 수 있음.
-  console.log("In another middleware");
-  res.send('<h1>The "Add product" Page</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
   // use를 사용하면 새로운 미들웨어를 추가할 수 있음.
-  console.log("In another middleware");
   res.send("<h1>Hello from Express!</h1>");
 });
 
