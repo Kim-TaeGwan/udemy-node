2f058774826bc8d55ab372c21ab58fd876afc768
kimtaegwan <ab154@naver.com>
2022-08-27 20:24:19 +0900

    Edit add-product function

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const rootPath = require("../utils/path");
+const rootDir = require("../utils/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.get("/", (req, res, next) => {
   // use를 사용하면 새로운 미들웨어를 추가할 수 있음.
   // path를 사용해서 절대경로로 파일 불러오기 가능
   // path.join()은 사용자의 운영체제를 감지하여 자동으로 올바른 경로를 생성한다.
-  res.sendFile(path.join(rootPath, "views", "shop.html"));
+  console.log("adminData.products : ", adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 module.exports = router;
