f9dfa7d31551242b952db0857bb38cb3a7146497
kimtaegwan <ab154@naver.com>
2022-08-27 19:39:25 +0900

    Add express.static

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public"))); // express.stati는 정적 파일 서비스
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
